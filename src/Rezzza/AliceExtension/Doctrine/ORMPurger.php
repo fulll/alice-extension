8930851ece4311d11b4feb9cfe6082095582f102
Kevin-koko <kevin.nerino@viacesi.fr>
2020-02-12 13:44:30 +0100

    speed-up ORMPurger

--- src/Rezzza/AliceExtension/Doctrine/ORMPurger.php
+++ src/Rezzza/AliceExtension/Doctrine/ORMPurger.php
@@ -112,12 +112,16 @@ class ORMPurger
             $entityManager->getConnection()->exec('SET foreign_key_checks = 0;');
         }
 
-        foreach ($tables as $tbl) {
-            if ($this->purgeMode === self::PURGE_MODE_DELETE) {
-                $entityManager->getConnection()->executeUpdate("DELETE IGNORE FROM " . $tbl);
-                $entityManager->getConnection()->executeUpdate("ALTER TABLE $tbl AUTO_INCREMENT = 1");
-            } else {
-                $entityManager->getConnection()->executeUpdate($platform->getTruncateTableSQL($tbl, true));
+        //speed-up db purge
+        if ($platform instanceof PostgreSqlPlatform && $this->purgeMode === self::PURGE_MODE_TRUNCATE && count($tables)) {
+            $entityManager->getConnection()->executeUpdate(sprintf('TRUNCATE %s CASCADE;', implode(',',$tables)));
+        } else {
+            foreach ($tables as $tbl) {
+                if ($this->purgeMode === self::PURGE_MODE_DELETE) {
+                    $entityManager->getConnection()->executeUpdate("DELETE IGNORE FROM " . $tbl);
+                } else {
+                    $entityManager->getConnection()->executeUpdate($platform->getTruncateTableSQL($tbl, true));
+                }
             }
         }
 
